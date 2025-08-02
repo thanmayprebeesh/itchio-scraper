2052eedd41c4558b6f238b422570f6d932467b66
RogueFiend <thanmayprebeesh@gmail.com>
2025-08-02 22:31:57 +0530

    Optimizing for Cloud Functions Commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const PORT = 8000;
 const express = require("express");
 
 const app = express();
@@ -14,5 +13,4 @@ app.use('/devlogs', require("./routes/devlogs"));
 app.use('/browse', require("./routes/browse"));
 app.use('/featured-jams', require("./routes/featured-jams"))
 
-app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
-
+exports.api = app;
